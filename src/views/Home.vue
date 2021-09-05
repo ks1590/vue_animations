25a282f2896cce96116b046c0a3eeb698f35d881
kensuke suzuki <35k.0101@gmail.com>
2021-09-05 19:14:06 +0900

    add:keyframes wobble

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -46,16 +46,27 @@ export default {
   .fade-leave-to{
     opacity: 0;
   }
+  
+  .toast-enter-active{
+    animation: wobble 0.5s ease;
+  }
 
-  .toast-enter-from,
   .toast-leave-to {
     opacity: 0;
-    transform: translateY(-60px);
-  }
-  
-  .toast-enter-active,
-  .toast-leave-active{
-    transition: all 0.3s ease;
+    transform: translateY(-60px)
   }
 
+  .toast-leave-active{
+    transition: all 0.3s ease;    
+  }
+
+  @keyframes wobble {
+    0% { transform: translateY(-60px); opacity: 0; }
+    50% { transform: translateY(0px); opacity: 1; }
+    60% { transform: translateX(8px); }
+    70% { transform: translateX(-8px); }
+    80% { transform: translateX(4px); }
+    90% { transform: translateX(-4px); }
+    100% { transform: translateX(0px); }
+  }
 </style>
