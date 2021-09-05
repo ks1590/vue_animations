dfaf6ca125c446622b0a6dac1307ae61f15b96bd
kensuke suzuki <35k.0101@gmail.com>
2021-09-05 19:05:04 +0900

    add:toast animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
+    <transition name="toast">
+      <Toast v-if="showToast" />    
+    </transition>
     <Todos @badValue="triggerToast" />
+    <transition name="fade">
+      <div v-if="showP">Hello,again</div>
+    </transition>
+
+    <button @click="showP = !showP">toggle</button>
   </div>
 </template>
 
@@ -14,17 +21,41 @@ export default {
   components: { Toast, Todos },
   setup() {
     const showToast = ref(false)
+    const showP = ref(false)
 
     const triggerToast = () => {
       showToast.value = true;
       setTimeout(() => showToast.value = false, 3000)
     }
 
-    return { showToast, triggerToast }
+    return { showToast, triggerToast, showP }
   }
 }
 </script>
 
 <style>
+  .fade-enter-from {
+    opacity: 0;
+  }
+  
+  .fade-enter-active,
+  .fade-leave-active{
+    transition: all 2s ease;
+  }
+  
+  .fade-leave-to{
+    opacity: 0;
+  }
+
+  .toast-enter-from,
+  .toast-leave-to {
+    opacity: 0;
+    transform: translateY(-60px);
+  }
+  
+  .toast-enter-active,
+  .toast-leave-active{
+    transition: all 0.3s ease;
+  }
 
 </style>
